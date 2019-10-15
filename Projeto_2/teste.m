16aaf1755ffdcad7a0d79fc0fc5184f209d1ddfc
danielsmorais <danielmorais@outlook.com.br>
2019-10-15 19:40:24 -0300

    .

---
+++ Projeto_2/teste.m
@@ -0,0 +1,34 @@
+n=5;      %number of state
+q=0.1;    %std of process 
+r=0.1;    %std of measurement
+Q=q^2*eye(n); % covariance of process
+R=r^2;        % covariance of measurement  
+
+theta = atan()
+
+for
+    
+end
+
+
+f=@(x)[x(2);x(3);0.05*x(1)*(x(2)+x(3));x(2);x(3)];  % nonlinear state equations
+h=@(x)x(1);                               % measurement equation
+s=[0;0;0;0;1];                                % initial state
+x=s+q*randn(n,1); %initial state          % initial state with noise
+P = eye(n);                               % initial state covraiance
+N=20;                                     % total dynamic steps
+xV = zeros(n,N);          %estmate        % allocate memory
+sV = zeros(n,N);          %actual
+zV = zeros(1,N);
+for k=1:N
+  z = h(s) + r*randn;                     % measurments
+  sV(:,k)= s;                             % save actual state
+  zV(k)  = z;                             % save measurment
+  [x, P] = ekf(f,x,P,h,z,Q,R);            % ekf 
+  xV(:,k) = x;                            % save estimate
+  s = f(s) + q*randn(n,1);                % update process 
+end
+for k=1:n                                 % plot results
+  subplot(n,1,k)
+  plot(1:N, sV(k,:), '-', 1:N, xV(k,:), '--')
+end
