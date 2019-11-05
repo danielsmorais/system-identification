7df0d2457e82426d6f73ec00e927e1ae886c0e6b
danielsmorais <danielmorais@outlook.com.br>
2019-11-05 00:34:48 -0300

    Finalizado.

--- Projeto_2/filtro_kalman_2D.m
+++ Projeto_2/filtro_kalman_2D.m
@@ -30,15 +30,14 @@ X = [0; 0; 0; 0; 0];
 P = zeros(5);
 % Variancia do ruido dinamico
 Q = diag([1 1 0.01 0.00001 0.00001])*10^-2;
-    % Percebi que o theta não afeta quase nada na filtragem
+    % Percebi que o theta nao afeta quase nada na filtragem
 
 % Matriz de medicao
 H = [1 0 0 0 0;
      0 1 0 0 0];
 
-
 % Variancia do ruido de medicao
-R = [0.1 0; 0 0.1]*10;
+R = [0.1 0; 0 0.1]*50;
 
 
 % Dados filtrados
@@ -49,12 +48,11 @@ theta = 0;
 v = 0;
 w = 0;
 
-
-% -- PRIMEIRA ITERAÇÃO-------------------------
+% -- PRIMEIRA ITERACAO-------------------------
 
 % Predicao
 PHI = [1 0 -sin(theta)*deltaT*v cos(theta)*deltaT 0;
-       0 1 cos(theta)*deltaT*v sin(theta)*deltaT*v 0;
+       0 1 cos(theta)*deltaT*v sin(theta)*deltaT 0;
        0 0 1 0 deltaT;
        0 0 0 1 0;
        0 0 0 0 1]; 
@@ -88,7 +86,7 @@ for i=2:npassos
     % Fase de predicao
  
     PHI = [1 0 -sin(theta)*deltaT*v cos(theta)*deltaT 0;
-           0 1 cos(theta)*deltaT*v sin(theta)*deltaT*v 0;
+           0 1 cos(theta)*deltaT*v sin(theta)*deltaT 0;
            0 0 1 0 deltaT;
            0 0 0 1 0;
            0 0 0 0 1];
@@ -105,7 +103,7 @@ for i=2:npassos
     X = X + K*(Y-H*X);
     P = P - K*H*P;
     
-    v = sqrt((data(i,3)-data(i-1,3))^2 + (data(i,2)-data(i-1,2))^2)/deltaT;
+    %v = sqrt((data(i,3)-data(i-1,3))^2 + (data(i,2)-data(i-1,2))^2)/deltaT;
     
     % salva todos os pontos --- tem que mutiplicar por H
     filtr(i,1) = X(1,1);
@@ -115,11 +113,11 @@ for i=2:npassos
     filtr(i,5) = X(5,1);
     
     theta = atan((filtr(i,2)-filtr(i-1,2))/(filtr(i,1)-filtr(i-1,1)));
-    if filtr(i,1)<filtr(i-1,1)
-        theta = theta + pi;
-    end  
+%     if filtr(i,1)<filtr(i-1,1)
+%         theta = theta + pi;
+%     end  
     
-    %v = sqrt((filtr(i,2)-filtr(i-1,2))^2 + (filtr(i,1)-filtr(i-1,1))^2)/deltaT;
+    v = sqrt((filtr(i,2)-filtr(i-1,2))^2 + (filtr(i,1)-filtr(i-1,1))^2)/deltaT;
 end
 
 % Percurso xy
