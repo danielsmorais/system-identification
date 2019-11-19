0f4968f5378cbc01dfc9a993eef405caf9bed8e9
danielsmorais <danielmorais@outlook.com.br>
2019-11-19 18:41:54 -0300

    .

---
+++ Projeto_3/filtro_particula.m
@@ -0,0 +1,17 @@
+% DANIEL SILVA DE MORAIS - 19/11/2019
+% FILTRO DE PARTÍCULAS PARA LOLIZAÇÃO DE VEÍCULO QUE UTILIZA DADOS DE GPS
+
+NUM_PARTICULA = 100;    
+
+% Conjnunto de particulas
+% uma particula é formada por [x y v theta_rua id_rua fator]
+particula = zeros(NUM_PARTICULA,6);
+
+
+% suposição que as partículas estão no centro da rua, sempre.
+% aplicar conceitos de genética
+% 
+
+
+
+
