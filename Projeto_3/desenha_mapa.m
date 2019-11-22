5ea16383b83dca0058263ab81f546276b38a1a4e
danielsmorais <danielmorais@outlook.com.br>
2019-11-22 15:34:28 -0300

    .

--- Projeto_3/desenha_mapa.m
+++ Projeto_3/desenha_mapa.m
@@ -1,5 +1,6 @@
 function desenha_mapa(mapa)
     for i=1:37
-        line([mapa(i,2) mapa(i,3)],[mapa(i,3) mapa(i,4)]);
-    end
+        line([mapa(i,2) mapa(i,4)],[mapa(i,3) mapa(i,5)]);
+    end    
+    axis equal
 end
