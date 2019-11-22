4ee7306e95c2d1d83b3b222b5072c4b3d5b6de01
danielsmorais <danielmorais@outlook.com.br>
2019-11-22 11:24:43 -0300

    Função para calcular distância entre ponto e reta.

---
+++ Projeto_3/dpr.m
@@ -0,0 +1,10 @@
+%DANIEL SILVA DE MORAIS - 22/11/2019
+%DISTANCIA ENTE PONTO E RETA
+%[d,x,y]: distancia e ponto de interseccao das retas perpendiculares
+%xp e yp: coordenada do ponto
+%a e b: parâmetros da reta : y = a*x+b
+function [d,x,y] = dpr(xp,yp,a,b)
+    x = ((1/a)*xp + yp - b)/(a + 1/a);
+    y = a*x+b;    
+    d = sqrt((x-xp)^2 + (y-yp)^2);    
+end
