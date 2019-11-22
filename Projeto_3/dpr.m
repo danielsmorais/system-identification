0e7b2aeb0f26b2dd733430b159bd5e2d105b3de6
danielsmorais <danielmorais@outlook.com.br>
2019-11-22 14:43:32 -0300

    Atualização da função.

--- Projeto_3/dpr.m
+++ Projeto_3/dpr.m
@@ -4,7 +4,17 @@
 %xp e yp: coordenada do ponto
 %a e b: parâmetros da reta : y = a*x+b
 function [d,x,y] = dpr(xp,yp,a,b)
-    x = ((1/a)*xp + yp - b)/(a + 1/a);
-    y = a*x+b;    
-    d = sqrt((x-xp)^2 + (y-yp)^2);    
+    if(a==Inf)
+        x = xp;
+        y = b;
+        d = abs(yp-b);
+    elseif(b==Inf)
+        x = a;
+        y = yp;
+        d = abs(xp-a);
+    else
+        x = ((1/a)*xp + yp - b)/(a + 1/a);
+        y = a*x+b;    
+        d = sqrt((x-xp)^2 + (y-yp)^2);         
+    end   
 end
