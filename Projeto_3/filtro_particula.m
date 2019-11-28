e577786784c221a4e9f5a981d4935d1462d98761
danielsmorais <danielmorais@outlook.com.br>
2019-11-28 17:26:49 -0300

    .

--- Projeto_3/filtro_particula.m
+++ Projeto_3/filtro_particula.m
@@ -9,13 +9,8 @@ mapa = load('mapa.txt');
 NUM_PARTICULA = 100;    
 
 % Conjnunto de particulas
-% uma particula é formada por [x y v theta_rua id_rua fator]
-%particula = zeros(NUM_PARTICULA,6);
-pf.x = 0;
-pf.y = 0;
-pf.v = 0;
-pf.w = 0;
-pf.idr = -1;
+% uma particula é formada por [x y v peso id_rua ]
+particula = zeros(NUM_PARTICULA, 5);
 
 
 % suposição que as partículas estão no centro da rua, sempre.
